25bb44256a7ade6ed3ea631642e0f567b1f6b4ce
Flyy-yu <peiyu94@gatech.edu>
2017-12-14 00:52:27 -0500

    more code

--- responder.py
+++ responder.py
@@ -1,30 +1,38 @@
 import hashlib
+from functions import *
 
 
 class responder(object):
     def __init__(self):
         self.potocal_name = b"Noise_xx()"
-        self.e_pub = ""
-        self.e_pri = ""
+        # ephemeral key pair
+        self.key = GENERATE_KEYPAIR()
+        self.e_pub = self.key.public_key()
+        self.e_pri = self.key
         self.re_pub = ""
         self.re_priv = ""
-        self.s = ""
+        # s_key: static key pair
+        self.s_key = GENERATE_KEYPAIR()
+        self.s_pub = self.s_key.public_key()
+        self.s_pri = self.s_key
         self.rs = ""
         self.h = ""
         self.ck = 0
         self.k = ""
         self.n = 0
+        self.ck1 = ""
+        self.ck2 = ""
 
     def first_stage(self):
-        self.h = hashlib.sha256()
-        self.h.update(self.potocal_name)
+        self.h = HASH(self.potocal_name)
+
         self.ck = self.h
 
         self.re_pub = b"This is fake sender's public key"
-        self.h.update(self.re_pub)
+        self.h = HASH(self.h + self.re_pub)
 
-        self.h.update(self.re_pub)
-        self.h.update(self.h)
+        self.h = HASH(self.h + self.re_pub)
+        self.h = HASH(self.h + self.h)
 
     # <------------ e, dhee,s,dhse
 
@@ -34,51 +42,50 @@ class responder(object):
         self.e_pub = b"This is fake responder's public key"
 
         # dhee
-
-        self.h.update(self.e_pub)
-        self.ck = "new ck from dhee with KDF"
-        self.k = "new key from dhee with FDK"
+        self.h = HASH(self.h + self.e_pub)
+        temp = DH(self.e_pri, self.re_pub)
+        # self.ck = "new ck from dhee with KDF"
+        # self.k = "new key from dhee with FDK"
+        self.ck, self.k = HKDF(self.ck, temp, 2)
         self.n = 0
 
         # s
-        self.s = b"This is the fake responder's static key"
+        self.s_pub = self.s_key.public_key()
 
-        cipher_to_sent = self.h + self.s
+        cipher_to_sent = self.h + self.s_pub
         cipher_to_sent = self.encrypt(self.k, cipher_to_sent)
-        self.h.update(cipher_to_sent)
+        self.h = HASH(self.h + cipher_to_sent)
 
         # dhse
-        self.ck = b"new ck from dhse with KDF"
-        self.k = b"new k from dhse with KDF"
+        # For "se": Calls MixKey(DH(s, re)) if initiator, MixKey(DH(e, rs)) otherwise.
+        # self.ck = b"new ck from dhse with KDF"
+        # self.k = b"new key from dhse with KDF"
+        temp = DH(self.s_pub, self.re_pub)
+        self.ck, self.k = HKDF(self.ck, temp, 2)
         self.n = 0
 
         # send payload encrypt with key? empty
         cipher_to_sent = self.h + b"sth"
         cipher_to_sent = self.encrypt(self.k, cipher_to_sent)
 
-        self.h.update(cipher_to_sent)
+        self.h = HASH(self.h + cipher_to_sent)
 
     def third_stage(self):
         # s
-        self.rs = b"this is the fake sender's static key"
+        self.rs = b"this is the fake sender's static key from sender"
+
         cipher_from_sender = b"123"
-        self.h.update(cipher_from_sender)
+        self.h = HASH(self.h + cipher_from_sender)
 
         # dhse
-        self.ck = b"new ck from dhse with KDF"
-        self.k = b"new key from dhse with KDF"
+        # For "se": Calls MixKey(DH(s, re)) if initiator, MixKey(DH(e, rs)) otherwise.
+        # self.ck = b"new ck from dhse with KDF"
+        # self.k = b"new key from dhse with KDF"
+        temp = DH(self.e_pub, self.rs)
 
-        self.ck1 = b"from ck"
-        self.ck2 = b"from ck"
+        # use KDF to get 2 key
+        self.ck1, self.ck2 = HKDF(self.ck, temp, 2)
 
-    def dhee(self):
-        return 1
-
-    def dhse(self):
-        return 1
-
-    def KDF(self):
-        return 1
 
     def encrypt(self, key, plain_text):
         return b"2"
--- sender.py
+++ sender.py
@@ -1,14 +1,21 @@
 import hashlib
+from functions import *
 
 
 class sender(object):
     def __init__(self):
         self.potocal_name = b"Noise_xx()"
-        self.e_pub = ""
-        self.e_pri = ""
+        # ephemeral key pair
+        self.key = GENERATE_KEYPAIR()
+        self.e_pub = self.key.public_key()
+        self.e_pri = self.key
         self.re_pub = ""
         self.re_priv = ""
-        self.s = ""
+        # s_key: static key pair
+        self.s_key = GENERATE_KEYPAIR()
+        self.s_pub = self.s_key.public_key()
+        self.s_pri = self.s_key
+
         self.rs = ""
         self.h = ""
         self.ck = 0
@@ -19,64 +26,60 @@ class sender(object):
 
 
     def first_stage(self):
-        self.h = hashlib.sha256()
-        self.h.update(self.potocal_name)
+        self.h = HASH(self.potocal_name)
         self.ck = self.h
-
-        self.e_pub = b"This is fake sender's public key"
-        self.e_priv = b"This is fake sender's private key"
 
         # sent to other side  e_pub-------->
         # the payload can be certificate, or empty
-        self.h.update(self.e_pub)
-        self.h.update(self.h)
+        self.h = HASH(self.h + self.e_pub)
+        self.h = HASH(self.h + self.h)
 
     # <------------ e, dhee,s,dhse
 
     def second_stage(self):
         "e"
         self.re_pub = b"This is fake responder's public key"
-        self.h.update(self.re_pub)
+        self.h = HASH(self.h + self.re_pub)
 
         # dhee?
-        self.ck = b"new ck from dhee with KDF"
-        self.k = b"new key from dhee with FDK"
+        temp = DH(self.e_pri, self.re_pub)
+        self.ck, self.k = HKDF(self.ck, temp, 2)
+        # self.ck = b"new ck from dhee with KDF"
+        # self.k = b"new key from dhee with FDK"
         self.n = 0
 
         # "S"
 
-        self.rs = b"This is the fake responder's static key"
+        self.rs = b"This is the fake responder's static key from responder"
         cipher_from_responder = b"123"
-        self.h.update(cipher_from_responder)
+        self.h = HASH(self.h + cipher_from_responder)
 
         # dhse
-        self.ck = b"new ck from dhse with KDF"
-        self.k = b"new key from dhse with KDF"
+        # For "se": Calls MixKey(DH(s, re)) if initiator, MixKey(DH(e, rs)) otherwise.
+        # self.ck = b"new ck from dhse with KDF"
+        # self.k = b"new key from dhse with KDF"
+        temp = DH(self.e_pub, self.rs)
+        self.ck, self.k = HKDF(self.ck, temp, 2)
         self.n = 0
 
     # --------------> s, dhse
 
     def third_stage(self):
         # s
-        self.s = b"this is the fake sender's static key"
+        self.s_pub = self.s_key.public_key()
         # sent it to responder
-        cipher_to_sent = self.h + self.s
+        cipher_to_sent = self.h + self.s_pub
         cipher_to_sent = self.encrypt(self.k, cipher_to_sent)
-        self.h.update(cipher_to_sent)
+        self.h = HASH(self.h + cipher_to_sent)
 
         # dhse
-        self.ck = b"new ck from dhse with KDF"
-        self.k = b"new key from dhse with KDF"
+        # For "se": Calls MixKey(DH(s, re)) if initiator, MixKey(DH(e, rs)) otherwise.
+        # self.ck = b"new ck from dhse with KDF"
+        # self.k = b"new key from dhse with KDF"
+        temp = DH(self.s_pub, self.re_pub)
+        self.ck, self.k = HKDF(self.ck, temp, 2)
+
         self.n = 0
-
-    def dhee(self):
-        return 1
-
-    def dhse(self):
-        return 1
-
-    def KDF(self):
-        return 1
 
     def encrypt(self, key, plain_text):
         return b"123"
